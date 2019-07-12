4336dc16cbcbbbc654718de4580d4ba2bb2bde6c
Bartek <bartoszlis87@gmail.com>
2019-07-12 09:55:31 +0200

    init

---
+++ precache-manifest.914da40fe783f61f9068520a325c191e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee1a2ccf9d3b6fea0762fea3a0db47b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf2195b4fabc2cbd5c3",
+    "url": "/static/css/main.54c39e50.chunk.css"
+  },
+  {
+    "revision": "8b90ec79b46625a79c17",
+    "url": "/static/js/2.0017e637.chunk.js"
+  },
+  {
+    "revision": "cbf2195b4fabc2cbd5c3",
+    "url": "/static/js/main.65f975ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec8d42769a833f2a840a6e978421a600",
+    "url": "/static/media/bg.ec8d4276.jpg"
+  },
+  {
+    "revision": "2d7fb50e1d64eb2704b367a952ecc380",
+    "url": "/static/media/logo.2d7fb50e.png"
+  },
+  {
+    "revision": "f8d962976858d4a33ff661d7bbe70c3a",
+    "url": "/static/media/myVideo.f8d96297.mp4"
+  }
+]);
